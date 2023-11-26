2d6a9a4641445fe5551a01141decdc7542a3c282
DRPPrashant <prashant.anand@derepu.com>
2023-11-26 21:33:48 +0530

    MaterialApp-Scaffold-Text

--- rolldice/lib/main.dart
+++ rolldice/lib/main.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() {
   //runApp() Its main job is actually to show some user interface
-  runApp(const MaterialApp(
-    home: Text(
-      'Hello Jiii!',
-      textAlign: TextAlign.center,
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Hello Jiii!',
+          ),
+        ),
+      ),
     ),
-  ));
+  );
 }
