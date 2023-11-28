21563fe198c027d2cb33aac0351b387eb71ca751
DRPPrashant <prashant.anand@derePu.com>
2023-11-29 00:08:52 +0530

    customWidget

---
+++ rolldice/lib/gradient_container.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  //super refers to parent class here is statelesswidget
+  //curley braces is used for passed name parameter
+
+  @override
+  Widget build(BuildContext context) {
+    //this build method will be executed by flutter for us
+    //flutter will call this method once its find your widget(i.e GradientContainer)
+    //inside of flutter widget or passed to the runApp function
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello Jiii!',
+          style: TextStyle(
+            fontSize: 28,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
