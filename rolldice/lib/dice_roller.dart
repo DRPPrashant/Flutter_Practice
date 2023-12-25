bb9c251698acbe0ba8e88e86aa66c0e9484be3ee
DRPPrashant <prashant.anand@derePu.com>
2023-12-25 23:21:58 +0530

    diceComplete02Way

--- rolldice/lib/dice_roller.dart
+++ rolldice/lib/dice_roller.dart
@@ -12,12 +12,11 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = 'assets/images/dice-2.png';
+  var currentDiceRoll = 2;
 
   void rollDice() {
-    var diceRoll = Random().nextInt(6) + 1;
     setState(() {
-      activeDiceImage = 'assets/images/dice-$diceRoll.png';
+      currentDiceRoll = Random().nextInt(6) + 1;
     });
 
     print('Changing Dice ....');
@@ -29,7 +28,7 @@ class _DiceRollerState extends State<DiceRoller> {
       mainAxisSize: MainAxisSize.min,
       children: [
         Image.asset(
-          activeDiceImage,
+          'assets/images/dice-$currentDiceRoll.png',
           width: 200,
         ),
         const SizedBox(height: 40),
