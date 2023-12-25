1f505de79994fbb8700ca225ed6e314fbd7e9d5c
DRPPrashant <prashant.anand@derePu.com>
2023-12-25 22:55:02 +0530

    dicenot_changes_statelessWidgetnotchangesui

--- rolldice/lib/gradient_container.dart
+++ rolldice/lib/gradient_container.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:rolldice/styled_text.dart';
 
 var startAlignment = Alignment.topLeft;
 var endAlignment = Alignment.bottomRight;
 
-
-//2nd way
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.color1, this.color2, {super.key}); //named parameter
+  GradientContainer(this.color1, this.color2, {super.key}); //named parameter
   //super refers to parent class here is statelesswidget
   //curley braces is used for passed name parameter
 
+  GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
   final Color color1;
   final Color color2;
+
+  var activeDiceImage = 'assets/images/dice-2.png';
+
+  void rollDice() {
+    activeDiceImage = 'assets/images/dice-4.png';
+    print('Changing Dice ....');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,47 +37,31 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyleText('Hello World!'),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              activeDiceImage,
+              width: 200,
+            ),
+            const SizedBox(height: 40),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(
+                //   top: 20,
+                // ),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                ),
+              ),
+              child: const Text('Roll Dice'),
+            )
+          ],
+        ),
       ),
     );
   }
 }
-
-
-
-// 1st Way: 
-
-// class GradientContainer extends StatelessWidget {
-//   const GradientContainer({super.key, required this.colors}); //named parameter
-//   //super refers to parent class here is statelesswidget
-//   //curley braces is used for passed name parameter
-
-//   final List<Color> colors;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     //this build method will be executed by flutter for us
-//     //flutter will call this method once its find your widget(i.e GradientContainer)
-//     //inside of flutter widget or passed to the runApp function
-
-//     // startAlignment = Alignment.center; we can reassigned
-//     return Container(
-//       decoration: BoxDecoration(
-//         gradient: LinearGradient(
-//           // colors: const [
-//           //   //by the const these color codes are locked here
-//           //   Color.fromARGB(255, 18, 6, 48),
-//           //   Color.fromARGB(255, 133, 126, 144)
-//           // ],
-//           colors: colors,
-//           begin: startAlignment,
-//           end: endAlignment,
-//         ),
-//       ),
-//       child: const Center(
-//         child: StyleText('Hello World!'),
-//       ),
-//     );
-//   }
-// }
--- rolldice/lib/main.dart
+++ rolldice/lib/main.dart
@@ -4,10 +4,13 @@ import 'package:rolldice/gradient_container.dart';
 void main() {
   //runApp() Its main job is actually to show some user interface
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: GradientContainer(Colors.black, Colors.yellow),
+        body: GradientContainer(
+          const Color.fromARGB(255, 33, 5, 100),
+          const Color.fromARGB(255, 68, 21, 149),
+        ),
       ),
     ),
   );
